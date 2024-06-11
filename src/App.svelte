26ca386789806591a174ce9935aa93ac6c78ced0
Laura Munsch <laura@kinoba.fr>
2024-06-11 17:56:52 +0200

    feat(activities): display activities styled based on distance, elevation gain & speed

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,26 @@
   import { authStore } from "@/stores";
 
   let firstname;
+  let picture;
 
-  authStore.subscribe((user) => (firstname = user.firstname));
+  authStore.subscribe((user) => {
+    firstname = user.firstname;
+    picture = user.picture;
+  });
 </script>
 
 <main>
   {#if firstname}
-    <h1>Bienvenue {firstname}</h1>
+    <div class="p-4 flex justify-between items-center">
+      <h1 class="text-2xl">Bienvenue {firstname}</h1>
+      {#if picture}
+        <img
+          src={picture}
+          alt="Portrait de {firstname}"
+          class="rounded-full w-24 h-24"
+        />
+      {/if}
+    </div>
 
     <Activities />
   {:else}
--- src/components/Activities.svelte
+++ src/components/Activities.svelte
@@ -4,18 +4,68 @@
 
   let activities;
 
+  onMount(() => loadActivities());
+
   const loadActivities = async () => {
     const { data } = await getActivities();
     if (data) activities = data;
+    console.log(activities);
   };
 
-  onMount(() => loadActivities());
+  const elevationGain = (activity) => {
+    return Math.floor(
+      (activity.total_elevation_gain / activity.distance) * 4000
+    );
+  };
+
+  const speed = (activity) => {
+    return Math.floor(activity.average_speed * 25 - 90);
+  };
+
+  const distance = (activity) => {
+    return Math.floor(activity.distance / 500);
+  };
 </script>
 
 {#if activities}
-  <ul>
+  <section class="flex flex-col items-center gap-4">
     {#each activities as activity}
-      <li>{activity.name}</li>
+      <div class="relative">
+        <div
+          class="activity"
+          style="width: {distance(activity)}px; height: {distance(
+            activity
+          )}px; opacity: {speed(activity)}%;
+            10}% 0% 0%);"
+        >
+          <div
+            class="activity__elevation-gain"
+            style="height: {elevationGain(activity)}%"
+          ></div>
+        </div>
+        <span
+          class="absolute bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-52 text-center hidden"
+          >{activity.name}</span
+        >
+      </div>
     {/each}
-  </ul>
+  </section>
 {/if}
+
+<style lang="scss">
+  .activity {
+    border-radius: 100%;
+    border: 1px solid theme("colors.black");
+    position: relative;
+    overflow: hidden;
+
+    &__elevation-gain {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: black;
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
